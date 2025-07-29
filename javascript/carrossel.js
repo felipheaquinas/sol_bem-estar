d3a7df2fff1716e818b75bdba5f91a9aa8cfccbc
Feliphe_De_Aquinas <feliphe.felix23@gmail.com>
2025-07-29 06:49:41 -0300

    Implementação de Carrossel

---
+++ javascript/carrossel.js
@@ -0,0 +1,19 @@
+const carrossel = document.getElementById('carrossel');
+let indice = 0;
+const totalImagens = carrossel.children.length;
+
+function atualizarCarrossel() {
+    carrossel.style.transform = `translateX(-${indice * 100}%)`;
+}
+
+function avancar() {
+    indice = (indice + 1) % totalImagens;
+    atualizarCarrossel();
+}
+
+function voltar() {
+    indice = (indice - 1 + totalImagens) % totalImagens;
+    atualizarCarrossel();
+}
+
+setInterval(avancar, 5000);
